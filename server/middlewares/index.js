c1975c16bd303faee5ff2b1b6ad69d149c1bd88e
Doluwamu <dboyadeitan@gmail.com>
2020-11-15 18:42:58 +0100

    made additional changes

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,6 @@
 exports.provideErrorHandler = (req, res, next) => {
-  res.sendApiError = (config) => {
-    const { status = 422, title, detail } = config;
+  res.sendApiError = ({ status = 422, title, detail }) => {
+    // const { status = 422, title, detail } = config;
     return res.status(status).send({
       errors: [{ title, detail }],
     });
