49b494a5d0ee7feef5710f56c4ace2b38b85b4e0
Doluwamu <dboyadeitan@gmail.com>
2020-11-27 22:19:20 +0100

    made additional changes

--- server/fakeDB/FakeDB.js
+++ server/fakeDB/FakeDB.js
@@ -1,16 +1,19 @@
-const { rentals, users } = require("./data");
+const { rentals, users, images } = require("./data");
 const Rental = require("../models/rental");
 const User = require("../models/users");
 const Booking = require("../models/booking");
+const CloudinaryImage = require("../models/cloudinary-image");
 
 class FakeDB {
   async clean() {
     await Rental.deleteMany({});
     await User.deleteMany({});
     await Booking.deleteMany({});
+    await CloudinaryImage.deleteMany({});
   }
 
   async addData() {
+    await CloudinaryImage.create(images);
     await Rental.create(rentals);
     await User.create(users);
   }
--- server/fakeDB/data/index.js
+++ server/fakeDB/data/index.js
@@ -1,8 +1,32 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const user1Id = mongoose.Types.ObjectId();
 const user2Id = mongoose.Types.ObjectId();
+
+const image1Id = mongoose.Types.ObjectId();
+const image2Id = mongoose.Types.ObjectId();
+const image3Id = mongoose.Types.ObjectId();
+
+exports.images = [
+  {
+    _id: image1Id,
+    cloudinaryId: "img2_sx2jlj",
+    url:
+      "https://res.cloudinary.com/book-with-me-react/image/upload/v1606513617/img2_sx2jlj.jpg",
+  },
+  {
+    _id: image2Id,
+    cloudinaryId: "img4_qqeon0",
+    url:
+      "https://res.cloudinary.com/book-with-me-react/image/upload/v1606513616/img4_qqeon0.jpg",
+  },
+  {
+    _id: image3Id,
+    cloudinaryId: "img3_u0l2mv",
+    url:
+      "https://res.cloudinary.com/book-with-me-react/image/upload/v1606513616/img3_u0l2mv.jpg",
+  },
+];
 
 exports.users = [
   {
@@ -25,39 +49,35 @@ exports.rentals = [
     city: "San Francisco",
     street: "Main street",
     category: "condo",
-    image:
-      "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+    image: image1Id,
     numOfRooms: 4,
     shared: true,
     description: "Very nice apartment in center of the city.",
     dailyPrice: 43,
-    owner: user1Id
+    owner: user1Id,
   },
   {
     title: "Modern apartment in center",
     city: "New York",
     street: "Time Square",
     category: "apartment",
-    image:
-      "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+    image: image2Id,
     numOfRooms: 1,
     shared: false,
     description: "Very nice apartment in center of the city.",
     dailyPrice: 11,
-    owner: user1Id
+    owner: user1Id,
   },
   {
     title: "Old house in nature",
     city: "Bratislava",
     street: "Letna 7",
     category: "house",
-    image:
-      "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+    image: image3Id,
     numOfRooms: 5,
     shared: true,
     description: "Very nice apartment in center of the city.",
     dailyPrice: 23,
-    owner: user2Id
+    owner: user2Id,
   },
 ];
-
